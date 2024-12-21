582a3944876334bc4f62b89aaf6c23f417ed80c9
LaxmiPrasanna <nagapurilaxmiprasanna04@gmail.com>
2024-12-21 10:34:25 +0530

    adding all the files to Git

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Apple from './Apple';
+import Mango from './Mango';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Apple name="laxmi" />
+      <Mango />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Mango.jsx
@@ -0,0 +1,6 @@
+function Mango(){
+    return (
+        <h1>Hi Am LaxmiPrasanna</h1>
+    )
+}
+export default  Mango;
